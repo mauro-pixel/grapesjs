7070284283a2d70a1efca170d566d7161eefed45
Mauro Espinal V <mauro@MacBook-Pro.local>
2020-05-20 19:17:36 -0500

    Ok

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -3,7 +3,7 @@ export default (editor, config) => {
   const toAdd = name => config.blocks.indexOf(name) >= 0;
 
   toAdd('link-block') && bm.add('link-block', {
-    category: 'Prueba',
+    category: 'Basic',
     label: 'Link Block',
     attributes: { class: 'fa fa-link' },
     content: {
@@ -21,7 +21,7 @@ export default (editor, config) => {
 
   toAdd('quote') && bm.add('quote', {
     label: 'Quote',
-    category: 'Prueba',
+    category: 'Basic',
     attributes: { class: 'fa fa-quote-right' },
     content: `<blockquote class="quote">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ipsum dolor sit
@@ -29,18 +29,8 @@ export default (editor, config) => {
   });
 
   toAdd('text-basic') && bm.add('text-basic', {
-    category: 'Prueba',
+    category: 'Basic',
     label: 'Text section',
-    attributes: { class: 'gjs-fonts gjs-f-h1p' },
-    content: `<section class="bdg-sect">
-      <h1 class="heading">Insert title here</h1>
-      <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-      </section>`
-  });
-
-  toAdd('navbar') && bm.add('navbar', {
-    category: 'Prueba',
-    label: 'Navbar',
     attributes: { class: 'gjs-fonts gjs-f-h1p' },
     content: `<section class="bdg-sect">
       <h1 class="heading">Insert title here</h1>
--- src/consts.js
+++ src/consts.js
@@ -1,10 +1,6 @@
 export const
-cmdImport = 'gjs-open-import-webpage',
-cmdDeviceDesktop = 'set-device-desktop',
-cmdDeviceTablet = 'set-device-tablet',
-cmdDeviceMobile = 'set-device-mobile',
-cmdClear = 'canvas-clear',
-hNavbarRef = 'h-navbar',
-navbarRef = 'navbar',
-navbarItemsRef = 'navbar-items',
-menuRef = 'navbar-menu';
+  cmdImport = 'gjs-open-import-webpage',
+  cmdDeviceDesktop = 'set-device-desktop',
+  cmdDeviceTablet = 'set-device-tablet',
+  cmdDeviceMobile = 'set-device-mobile',
+  cmdClear = 'canvas-clear';
